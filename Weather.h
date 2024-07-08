ac2ac4c22504318cca33c0782e87ea88e92cfe8f
Miggy1206 <miguelsbarbosa123@gmail.com>
2024-07-08 22:56:28 +0100

    Weather class work

--- Weather.h
+++ Weather.h
@@ -14,5 +14,6 @@ public:
 	void set_coordinates(double lat, double lon);
 	void set_weather_data(Value data);
 	void display_weather_data();
+	string weather_url_parser(string url_start, vector<string> chosen_parameters);
 };
 
